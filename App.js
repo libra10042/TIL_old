b19c3bd9fd6d529bb88cd35b012fd022c89e41f8
libra10042 <libra10042@gmail.com>
2020-10-09 05:56:03 +0900

    sass

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-// import React, { Component } from "react";
-// import "./App.css";
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1 className="title">두잇! 리액트 시작하기</h1>
-//       </div>
-//     );
-//   }
-// }
+import React, { Component } from "react";
+import "./App.css";
 
-// export default App;
-import React from "react";
-// create-react-app은 js 또는 jsx 파일의 확장자를 생략해도 해당 파일을 자동으로 찾을 수 있게 설정되었다.
-import TodaysPlan from "./03/TodaysPlan";
-
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="body">
-        <TodaysPlan />
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <div>두잇 리액트 시작하기</div>
+          </div>
+        </nav>
+        <h1>머티리얼 css</h1>
       </div>
     );
   }
 }
-
 export default App;
--- stories/InputStory.jsx
+++ stories/InputStory.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 
 import Input from "../03/Input";
 
-storiesOf("Input", module)
+storiesOf("input", module)
   .addWithJSX("기본설정", () => <Input name="name" />)
-  .addWithJSX("label 예제", () => <Input name="name" label="이름" />)
-  .addWithJSX("onChange 예제", () => 
-    <input name="name" onChange={action("onChange 이벤트 발생")} />
-  );
+  .addWithJSX("label 예제", () => <input type name="name" label="이름" />)
+  .addWithJSX("onChange 예제", () => (
+    <Input name="name" onChange={action("onChange 이벤트 발생")} />
+  ));
