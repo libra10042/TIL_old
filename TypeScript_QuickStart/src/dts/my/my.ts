3e6e4b953a737a474d2ea42205f8cc76c700bda8
libra10042 <libra10042@gmail.com>
2020-10-24 21:40:24 +0900

    3rd party lib  and  type definition file

---
+++ TypeScript_QuickStart/src/dts/my/my.ts
@@ -0,0 +1,10 @@
+export namespace MyLibrary { 
+    export function getMaxNumber(array : number[]) :  number{ 
+        return Math.max.apply(Math, array);
+    }
+
+
+    function getMinNumber(array : number[]) : number {
+        return Math.max.apply(Math, array);
+    }
+}
