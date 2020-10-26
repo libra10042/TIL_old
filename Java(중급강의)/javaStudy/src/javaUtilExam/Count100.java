b1e96ef2060150156efe66f6362d9326cfad0d92
libra10042 <libra10042@gmail.com>
2020-10-26 22:05:52 +0900

    Java annotation create and use

---
+++ Java(중급강의)/javaStudy/src/javaUtilExam/Count100.java
@@ -0,0 +1,10 @@
+package javaUtilExam;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Count100 {
+
+	
+}
