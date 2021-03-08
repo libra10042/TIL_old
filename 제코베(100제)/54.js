6d3e0cfdfd79c625e3e8031dee563b82bbbb7103
libra10042 <libra10042@gmail.com>
2021-03-08 15:01:29 +0900

    연속되는 수

--- 제코베(100제)/54.js
+++ 제코베(100제)/54.js
@@ -19,13 +19,12 @@ NO
 function solution(arr){
     arr.sort((a,b) => {return a-b});
 
-    for(let i=0; i<=arr.length-1; i++){
+    for(let i=0; i<arr.length-1; i++){
         if(arr[i] + 1 !== arr[i+1] ){
             return 'No';
-        }else{
-            return 'YES'
         }
     }
+    return 'YES';
 }
 
 const n = '1 3 4 2 5 6'.split(' ')
