bb8a9f43e8fd45ee5cf42501ae155b7fda6e96ea
libra10042 <libra10042@gmail.com>
2021-06-11 22:20:00 +0900

    Array method

--- JS로하는자료구조알고리즘 (도서)/5.Array1_ex.js
+++ JS로하는자료구조알고리즘 (도서)/5.Array1_ex.js
@@ -82,3 +82,49 @@ function medianOfArray(array){
     }
 }
 
+// ----------------------------------------------------------------------
+function medianOfArray(array){
+    let length = array.length; 
+    // 홀수 
+    if(length % 2 == 1){
+        return array[Math.floor(length/2)];
+    }else{
+        //짝수 
+        return (array[length /2]  + array[length / 2 - 1]) / 2; 
+    }
+}
+// arr2이 더 큰 배열이다.
+function medianOfTwoSortedArray(arr1, arr2, pos){
+    if(pos <= 0){
+        return -1; 
+    }
+    if(pos == 1){
+        return (arr1[0] + arr2[0])/ 2; 
+    }
+    if(pos == 2){
+        return (Math.max(arr1[0], arr2[0]) + Math.min(arr[1], arr2[1])) / 2; 
+    }
+
+
+    let median1 = medianOfArray(arr1);
+    let median2 = medianOfArray(arr2);
+
+    if(median1 == median2){
+        return median1; 
+    }
+
+    let evenOffset = pos % 2 == 0 ? 1 : 0,
+        offsetMinus = Math.floor(pos / 2) - evenOffset,
+        offsetPlus = pos - Math.floor(pos / 2) + evenOffset; 
+
+
+    if(median1 < median2){
+        return medianOfTwoSortedArray(arr1.slice(offsetMinus),
+            arr2.slice(0, offsetMinus), offsetPlus);
+    }else {
+        return medianOfTwoSortedArray(arr2.slice(offsetMinus),
+            arr1.slice(0, -offsetMinus), offsetPlus);
+    }
+
+}
+
